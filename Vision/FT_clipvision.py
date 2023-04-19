59f310157812734bc421f907f1c0448dbe9c374f
yang-zj1026 <victoryang1026@gmail.com>
2023-04-19 11:57:31 -0700

    Minor update

--- Vision/FT_clipvision.py
+++ Vision/FT_clipvision.py
@@ -1,55 +1,28 @@
-from transformers import AutoProcessor, CLIPModel, CLIPTextModel, CLIPVisionModel, AutoTokenizer
-from transformers.adapters import AdapterConfig, PrefixTuningConfig, LoRAConfig, IA3Config, MAMConfig, UniPELTConfig
-from torchvision.datasets import Caltech101, CIFAR10, CIFAR100
 import os
-from tqdm import tqdm
+import time
+
 import torch
+from torchvision.datasets import CIFAR100
+from tqdm import tqdm
+from transformers import AutoProcessor
+from transformers.adapters import AdapterConfig, PrefixTuningConfig, LoRAConfig, IA3Config, MAMConfig, UniPELTConfig
+from timm.utils import AverageMeter
+
 from Visionmodel import CLIPVisionClassification
-import logging
+from utils import create_logger
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.FileHandler('log.txt')
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
-device = "cuda:6"
-
-processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch16")
-
-configs = {
-    "adapter": AdapterConfig(mh_adapter=True, output_adapter=True, reduction_factor=16, non_linearity="relu"),
-    "prefix": PrefixTuningConfig(flat=False, prefix_length=30),
-    "LoRA": LoRAConfig(r=8, alpha=16),
-    "IA3": IA3Config(),
-    "mam": MAMConfig(),
-    "unipelt": UniPELTConfig()
-}
-
-for configname,config in configs.items():
-    trainset = CIFAR100(root=os.path.expanduser("~/.cache"), train=True, download=True)
-    testset = CIFAR100(root=os.path.expanduser("~/.cache"), train=False, download=True)
-
-    model = CLIPVisionClassification(768, len(trainset.classes), 0.2)
-
-    model.clipvision.add_adapter(configname, config=config)
-    model.clipvision.train_adapter(configname)
-    model.clipvision.set_active_adapters(configname)
-    model.to(device)
-
+def train(train_set, test_set, processor, model, configname, batch_size, epochs):
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=5e-5)
 
-    batch_size = 32
-
-    for _ in range(10):
-        total = 0
-        cnt = 0
-        bz = 0
-        tmp_img = []
-        tmp_label = []
-        with tqdm(trainset, desc='train_epoch{}_adapter_{}'.format(_, configname)) as loop:
+    for epoch in range(epochs):
+        # Train
+        total, cnt, bz = 0, 0, 0
+        train_loss = AverageMeter()
+        tmp_img, tmp_label = [], []
+        model.train()
+        with tqdm(train_set, desc='train_epoch{}_adapter_{}'.format(epoch, configname)) as loop:
             for image, class_id in loop:
                 bz += 1
                 if bz <= batch_size:
@@ -68,18 +41,21 @@ for configname,config in configs.items():
                     if loss != 0:
                         loss.backward()
                     optimizer.step()
+
+                    train_loss.update(loss.item(), len(tmp_label))
+                    loop.set_postfix(loss=train_loss.avg, acc=cnt / total)
+
                     bz = 0
                     tmp_img = []
                     tmp_label = []
-                    loop.set_postfix(loss=loss.item(), acc=cnt / total)
-        logger.info('train_epoch{}_adapter_{}, acc:{}'.format(_, configname, cnt / total))
+        logger.info('train_epoch{}_adapter_{}, acc:{}'.format(epoch, configname, cnt / total))
 
-        total = 0
-        cnt = 0
-        bz = 0
+        # Evaluate
+        total, cnt, bz = 0, 0, 0
         tmp_img = []
         tmp_label = []
-        with tqdm(testset, desc='test_epoch{}_adapter_{}'.format(_,configname)) as loop:
+        model.eval()
+        with tqdm(test_set, desc='test_epoch{}_adapter_{}'.format(epoch, configname)) as loop:
             for image, class_id in loop:
                 bz += 1
                 if bz <= batch_size:
@@ -98,6 +74,43 @@ for configname,config in configs.items():
                     tmp_label = []
 
                     loop.set_postfix(acc=cnt / total)
-        logger.info('test_epoch{}_adapter_{}, acc:{}'.format(_, configname, cnt / total))
+        logger.info('test_epoch{}_adapter_{}, acc:{}'.format(epoch, configname, cnt / total))
 
     model.clipvision.save_adapter("./{}".format(configname), "{}".format(configname))
+
+
+if __name__ == '__main__':
+    processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch16")
+
+    trainset = CIFAR100(root=os.path.expanduser("~/.cache"), train=True, download=True)
+    testset = CIFAR100(root=os.path.expanduser("~/.cache"), train=False, download=True)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"  #
+    train_epochs = 20
+
+    configs = {
+        "adapter": AdapterConfig(mh_adapter=True, output_adapter=True, reduction_factor=16, non_linearity="relu"),
+        "prefix": PrefixTuningConfig(flat=False, prefix_length=30),
+        "LoRA": LoRAConfig(r=8, alpha=16),
+        "IA3": IA3Config(),
+        "mam": MAMConfig(),
+        "unipelt": UniPELTConfig()
+    }
+
+    for configname, config in configs.items():
+        # logger and model saved dir
+        timestamp = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime())
+        output_dir = os.path.join('./Log', f'{configname}_finetune_{timestamp}')
+        os.makedirs(output_dir, exist_ok=True)
+        logger = create_logger(os.path.join(output_dir, "finetune_clipvision_log_" + timestamp + ".txt"),
+                               add_stream=False)
+        print('Save path:', output_dir)
+
+        model = CLIPVisionClassification(768, len(trainset.classes), 0.2)
+
+        model.clipvision.add_adapter(configname, config=config)
+        model.clipvision.train_adapter(configname)
+        model.clipvision.set_active_adapters(configname)
+        model.to(device)
+
+        train(trainset, testset, processor, model, configname, 32, train_epochs)
