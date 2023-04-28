52c395233d73942fa3b3c5596e8ae5b4bcd5a78e
Aika <zheyizhu@usc.edu>
2023-04-28 02:17:59 -0700

    Update Visionmodel.py

--- train_eval/Visionmodel.py
+++ train_eval/Visionmodel.py
@@ -5,6 +5,8 @@ from transformers import AutoProcessor, CLIPModel, CLIPTextModel, CLIPVisionMode
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
 import torch.nn.functional as F
+import torch.distributed as dist
+
 import numpy as np
 from transformers.adapters import AdapterConfig, PrefixTuningConfig, LoRAConfig, IA3Config, MAMConfig, UniPELTConfig
 
@@ -38,6 +40,32 @@ class CLIPOutput:
             'mask': mask
         }
 
+class GatherLayer(torch.autograd.Function):
+    """
+    Gather tensors from all workers with support for backward propagation:
+    This implementation does not cut the gradients as torch.distributed.all_gather does.
+    """
+    @staticmethod
+    def forward(ctx, x):
+        output = [torch.zeros_like(x) for _ in range(dist.get_world_size())]
+        dist.all_gather(output, x)
+        return tuple(output)
+    @staticmethod
+    def backward(ctx, *grads):
+        all_gradients = torch.stack(grads)
+        dist.all_reduce(all_gradients)
+        return all_gradients[dist.get_rank()]
+    
+def gather_features(
+        image_features,
+        text_features,
+):
+    gathered_image_features = GatherLayer.apply(image_features)
+    gathered_text_features = GatherLayer.apply(text_features)
+    all_image_features = torch.cat(gathered_image_features)
+    all_text_features = torch.cat(gathered_text_features)
+
+    return all_image_features, all_text_features
 
 # The implementation code is modified from open_clip (https://github.com/mlfoundations/open_clip.git)
 class ClipLoss(nn.Module):
@@ -58,16 +86,16 @@ class ClipLoss(nn.Module):
 
     def forward(self, image_features, text_features, logit_scale):
         device = image_features.device
-        # if self.world_size > 1:
-        #     all_image_features, all_text_features = gather_features(
-        #         image_features, text_features
-        #     )
+        if self.world_size > 1:
+            all_image_features, all_text_features = gather_features(
+                image_features, text_features
+            )
 
-        #     logits_per_image = logit_scale * image_features @ all_text_features.T
-        #     logits_per_text = logit_scale * text_features @ all_image_features.T
-        # else:
-        logits_per_image = logit_scale * image_features @ text_features.T
-        logits_per_text = logit_scale * text_features @ image_features.T
+            logits_per_image = logit_scale * image_features @ all_text_features.T
+            logits_per_text = logit_scale * text_features @ all_image_features.T
+        else:
+            logits_per_image = logit_scale * image_features @ text_features.T
+            logits_per_text = logit_scale * text_features @ image_features.T
 
         # calculated ground-truth and cache if enabled
         num_logits = logits_per_image.shape[0]
@@ -82,9 +110,9 @@ class ClipLoss(nn.Module):
             labels = self.labels[device]
 
         total_loss = (
-                             F.cross_entropy(logits_per_image, labels, reduction='none') +
-                             F.cross_entropy(logits_per_text, labels, reduction='none')
-                     ) / 2
+            F.cross_entropy(logits_per_image, labels) +
+            F.cross_entropy(logits_per_text, labels)
+            ) / 2
         return total_loss, logits_per_image, logits_per_text
 
 
@@ -112,12 +140,6 @@ class CLIPVisionMasked(nn.Module):
     def __init__(self, dropout_rate):
         super().__init__()
         self.clipvision = CLIPVisionModelWithProjection.from_pretrained("openai/clip-vit-base-patch16")
-        self.vision_model = self.clipvision.vision_model
-
-        self.vision_embedding = self.clipvision.vision_model.embeddings
-        self.patch_embedding = self.clipvision.vision_model.embeddings.patch_embedding
-        self.class_embedding = self.clipvision.vision_model.embeddings.class_embedding
-        self.position_embedding = self.clipvision.vision_model.embeddings.position_embedding
 
         self.embed_dim = self.clipvision.vision_model.config.hidden_size
 
@@ -128,7 +150,7 @@ class CLIPVisionMasked(nn.Module):
         if mask is None:
             raise ValueError("The mask is None")
 
-        patch_embeds = self.patch_embedding(pixel_values)
+        patch_embeds = self.clipvision.vision_model.embeddings.patch_embedding(pixel_values)
         patch_embeds = patch_embeds.flatten(2).transpose(1, 2)
         B, L, _, = patch_embeds.shape
 
@@ -137,19 +159,19 @@ class CLIPVisionMasked(nn.Module):
         w = mask.flatten(1).unsqueeze(-1).type_as(mask_token)
         patch_embeds = patch_embeds * (1 - w) + mask_token * w
 
-        class_embeds = self.class_embedding.expand(B, 1, -1)
+        class_embeds = self.clipvision.vision_model.embeddings.class_embedding.expand(B, 1, -1)
         embeddings = torch.cat((class_embeds, patch_embeds), dim=1)
-        embeddings = embeddings + self.position_embedding(self.vision_embedding.position_ids)
+        embeddings = embeddings + self.clipvision.vision_model.embeddings.position_embedding(self.clipvision.vision_model.embeddings.position_ids)
 
-        embeddings = self.vision_model.pre_layrnorm(embeddings)
+        embeddings = self.clipvision.vision_model.pre_layrnorm(embeddings)
         embeddings = self.pos_drop(embeddings)
 
-        encoder_outputs = self.vision_model.encoder(inputs_embeds=embeddings)
+        encoder_outputs = self.clipvision.vision_model.encoder(inputs_embeds=embeddings)
         last_hidden_state = encoder_outputs[0]
         outputs = last_hidden_state[:, 1:, :]
-        img_emb = self.vision_model.post_layernorm(outputs)  # [64, 196, 768]
+        img_emb = self.clipvision.vision_model.post_layernorm(outputs)  # [64, 196, 768]
         pooled_output = last_hidden_state[:, 0, :]
-        pooled_output = self.vision_model.post_layernorm(pooled_output)
+        pooled_output = self.clipvision.vision_model.post_layernorm(pooled_output)
 
         B, L, C = img_emb.shape
         H = W = int(L ** 0.5)
@@ -158,36 +180,16 @@ class CLIPVisionMasked(nn.Module):
         return img_emb, pooled_output
     
     def forward_unmasked(self, pixel_values):
-
-        patch_embeds = self.patch_embedding(pixel_values)
-        patch_embeds = patch_embeds.flatten(2).transpose(1, 2)
-        B, L, _, = patch_embeds.shape
-
-        class_embeds = self.class_embedding.expand(B, 1, -1)
-        embeddings = torch.cat((class_embeds, patch_embeds), dim=1)
-        embeddings = embeddings + self.position_embedding(self.vision_embedding.position_ids)
-
-        embeddings = self.vision_model.pre_layrnorm(embeddings)
-        embeddings = self.pos_drop(embeddings)
-
-        encoder_outputs = self.vision_model.encoder(inputs_embeds=embeddings)
-        last_hidden_state = encoder_outputs[0]
-        outputs = last_hidden_state[:, 1:, :]
-        img_emb = self.vision_model.post_layernorm(outputs)  # [64, 196, 768]
-        pooled_output = last_hidden_state[:, 0, :]
-        pooled_output = self.vision_model.post_layernorm(pooled_output)
-
-        B, L, C = img_emb.shape
-        H = W = int(L ** 0.5)
-        img_emb = img_emb.permute(0, 2, 1).reshape(B, C, H, W)  # [64, 768, 14, 14]
-
-        return img_emb, pooled_output
+        inputs = {}
+        inputs["pixel_values"] = pixel_values
+        image_embeds = self.clipvision(**inputs).image_embeds
+        return image_embeds
 
     def forward(self, pixel_values, mask):
         masked_img_emb,masked_pooled_output = self.forward_masked(pixel_values, mask)
-        unmasked_img_emb, unmasked_pooled_output = self.forward_unmasked(pixel_values)
+        # image_embeds = self.forward_unmasked(pixel_values)
         
-        return masked_img_emb, unmasked_pooled_output
+        return masked_img_emb#, image_embeds
         
 
 
@@ -217,7 +219,7 @@ class CLIPWeightedLOSS(nn.Module):
         self.encoder.clipvision.add_adapter(vision_configname, config=configs[vision_configname])
         self.encoder.clipvision.train_adapter(vision_configname)
         self.encoder.clipvision.set_active_adapters(vision_configname)
-        self.visual_projection = self.encoder.clipvision.visual_projection#nn.Linear(768, 512, bias=False)
+        # self.visual_projection = self.encoder.clipvision.visual_projection#nn.Linear(768, 512, bias=False)
 
         text_configname = "LoRA"
         self.cliptext = CLIPTextModelWithProjection.from_pretrained("openai/clip-vit-base-patch16")
@@ -229,6 +231,8 @@ class CLIPWeightedLOSS(nn.Module):
         self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
         self.mask_generator = MaskGenerator(input_size=224, mask_patch_size=32,
                                             model_patch_size=16, mask_ratio=0.6)
+        
+
 
     def forward(self, input_ids, pixel_values, attention_mask):
         # device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -237,20 +241,25 @@ class CLIPWeightedLOSS(nn.Module):
         mask = torch.tensor(np.array(mask)).to(pixel_values.device)
         # print('pixel:', pixel_values.device, 'mask', mask.device)
 
-        z, pooled_output = self.encoder(pixel_values, mask)
+        z = self.encoder(pixel_values, mask)
         x_rec = self.decoder(z)
-        vision_outputs = self.visual_projection(pooled_output)
+        # vision_outputs = self.visual_projection(pooled_output)
 
         text_input = {}
         text_input['input_ids'] = input_ids
         text_input['attention_mask'] = attention_mask
         text_outputs = self.cliptext(**text_input)
 
-        image_embeds = vision_outputs / vision_outputs.norm(p=2, dim=-1, keepdim=True)
-        text_embeds = text_outputs.text_embeds / text_outputs.text_embeds.norm(p=2, dim=-1, keepdim=True)
 
-        clip_loss, logits_per_image, logits_per_text = self.criterion(image_embeds, text_embeds, self.logit_scale.exp())
-        clip_loss = torch.mean(clip_loss)
+        vision_input = {}
+        vision_input['pixel_values'] = pixel_values
+        vision_outputs = self.encoder.clipvision(**vision_input)
+
+
+        image_embeds = vision_outputs.image_embeds #/ vision_outputs.image_embeds.norm(p=2, dim=-1, keepdim=True)
+        text_embeds = text_outputs.text_embeds #/ text_outputs.text_embeds.norm(p=2, dim=-1, keepdim=True)
+
+        clip_loss, logits_per_image, logits_per_text = self.criterion(image_embeds, text_embeds, self.logit_scale)
 
         mask = mask.repeat_interleave(16, 1).repeat_interleave(16, 2).unsqueeze(1).contiguous()
         loss_recon = F.l1_loss(pixel_values, x_rec, reduction='none')
